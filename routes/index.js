7ed72a3039c6779cdf7f3148d054c8231937761f
Benson Clark <benson@whitemb.local>
2016-01-01 15:38:14 -0700

    View skeleton/routing

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,67 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('main');
+  res.render('./menus/main.ejs');
 });
 
 router.get('/league', function(req, res, next) {
-  res.render('league');
+  res.render('./league/league.ejs');
 });
 
 router.get('/game', function(req, res, next) {
-  res.render('game');
+  res.render('./game/game.ejs');
 });
 
 router.get('/playoffs', function(req, res, next) {
-  res.render('playoffs');
+  res.render('./schedule/playoffs.ejs');
 });
 
 router.get('/playerCareer', function(req, res, next) {
-  res.render('playerCareer');
+  res.render('./menus/playerCareer.ejs');
 });
 
 router.get('/coachCareer', function(req, res, next) {
-  res.render('coachCareer');
+  res.render('./menus/coachCareer.ejs');
+});
+
+router.get("/meetings/gm", function(req, res, next){
+	res.render('./meetings/gm.ejs');
+});
+
+router.get("/meetings/coach", function(req, res, next){
+	res.render('./meetings/coach.ejs');
+});
+
+router.get("/meetings/player", function(req, res, next){
+	res.render('./meetings/player.ejs');
+});
+
+router.get("/meetings/agent", function(req, res, next){
+	res.render('./meetings/agent.ejs');
+});
+
+router.get("/schedule/team", function(req, res, next){
+	res.render('./schedule/team.ejs');
+});
+
+router.get("/game", function(req, res, next){
+	res.render('./game/game.ejs');
+});
+
+router.get("/team/roster", function(req, res, next){
+	res.render('./team/roster.ejs');
+});
+
+router.get("/team/depthchart", function(req, res, next){
+	res.render('./team/depthchart.ejs');
+});
+
+router.get("/league/news", function(req, res, next){
+	res.render('./league/news.ejs');
+});
+
+router.get("/league/leaders", function(req, res, next){
+	res.render('./league/leaders.ejs');
 });
 
 module.exports = router;
